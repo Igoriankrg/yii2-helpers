e84d2e2af5aa7fd5172f2f56e77d81eb3d229ea5
Vitaliy <theyamshikov@yandex.ru>
2017-09-19 16:09:25 +0600

    add helpers

---
+++ src/UrlHelper.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace yii2lab\helpers;
+
+use yii\helpers\Url;
+
+class UrlHelper {
+	
+	static function generateUrl($url, $getParameters = null) {
+		$url = Url::to([$url]);
+		if(!empty($getParameters)) {
+			$get = self::generateGetParameters($getParameters);
+			if(!empty($get)) {
+				$url .= '?' . $get;
+			}
+		}
+		return $url;
+	}
+	
+	static function generateGetParameters($params) {
+		$result = '';
+		foreach($params as $name => $value) {
+			$result .= "&$name=$value";
+		}
+		$result = trim($result, '&');
+		return $result;
+	}
+	
+}
