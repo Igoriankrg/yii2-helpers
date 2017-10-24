29bd150e2093d4096887c9136cc05d539c70c7a3
Vitaliy <theyamshikov@yandex.ru>
2017-10-25 02:02:24 +0600

    fix no image

--- src/yii/Html.php
+++ src/yii/Html.php
@@ -2,15 +2,22 @@
 
 namespace yii2lab\helpers\yii;
 
+use Yii;
 use yii\helpers\Html as YiiHtml;
 
 class Html extends YiiHtml
 {
+	const WEB_NO_IMAGE = '@web/images/image/no_image.png';
+
 	public static function getDataUrl($fileName) {
-		$content = FileHelper::load($fileName);
-		$mimeType = FileHelper::getMimeType($fileName);
-		$base64code = 'data:'.$mimeType.';base64, ' . base64_encode($content);
-		return $base64code;
+		$fileName = FileHelper::normalizePath($fileName);
+		if(FileHelper::has($fileName)) {
+			$content = FileHelper::load($fileName);
+			$mimeType = FileHelper::getMimeType($fileName);
+			$base64code = 'data:'.$mimeType.';base64, ' . base64_encode($content);
+			return $base64code;
+		}
+		return Yii::getAlias(self::WEB_NO_IMAGE);
 	}
 
 	public static function fa($icon, $options = [], $prefix = 'fa fa-', $tag = 'i')
