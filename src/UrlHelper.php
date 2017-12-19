86f77cf569eb55e717551d07e599343247cd7826
Vitaliy <theyamshikov@yandex.ru>
2017-12-19 20:14:18 +0600

    update method

--- src/UrlHelper.php
+++ src/UrlHelper.php
@@ -6,12 +6,16 @@ use yii\helpers\Url;
 
 class UrlHelper {
 	
-	static function isAbsolute($url) {
-		return preg_match('#(https?:)#', $url);
+	public static function isAbsolute($url) {
+		$pattern = "/^(?:ftp|https?|feed)?:?\/\/(?:(?:(?:[\w\.\-\+!$&'\(\)*\+,;=]|%[0-9a-f]{2})+:)*
+        (?:[\w\.\-\+%!$&'\(\)*\+,;=]|%[0-9a-f]{2})+@)?(?:
+        (?:[a-z0-9\-\.]|%[0-9a-f]{2})+|(?:\[(?:[0-9a-f]{0,4}:)*(?:[0-9a-f]{0,4})\]))(?::[0-9]+)?(?:[\/|\?]
+        (?:[\w#!:\.\?\+\|=&@$'~*,;\/\(\)\[\]\-]|%[0-9a-f]{2})*)?$/xi";
+		return (bool) preg_match($pattern, $url);
 	}
 	
-	static function generateUrl($url, $getParameters = null) {
-		$url = Url::to([$url]);
+	public static function generateUrl($url, $getParameters = null) {
+			$url = Url::to([$url]);
 		if(!empty($getParameters)) {
 			$get = self::generateGetParameters($getParameters);
 			if(!empty($get)) {
@@ -21,7 +25,7 @@ class UrlHelper {
 		return $url;
 	}
 	
-	static function generateGetParameters($params) {
+	public static function generateGetParameters($params) {
 		$result = '';
 		foreach($params as $name => $value) {
 			$result .= "&$name=$value";
