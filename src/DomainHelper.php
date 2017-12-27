d588f1a476535a4a6e32add2f4ef2b36d13156f3
Vitaliy <theyamshikov@yandex.ru>
2017-12-28 01:05:09 +0600

    make method

--- src/DomainHelper.php
+++ src/DomainHelper.php
@@ -3,10 +3,22 @@
 namespace yii2lab\helpers;
 
 use Yii;
+use yii2lab\app\domain\Domain;
 use yii2lab\helpers\yii\ArrayHelper;
 
 class DomainHelper
 {
+	
+	public static function has($name) {
+		if(!Yii::$app->has($name)) {
+			return false;
+		}
+		$domain = !Yii::$app->get($name);
+		if(!$domain instanceof Domain) {
+			return false;
+		}
+		return true;
+	}
 	
 	public static function messagesAlias($bundleName) {
 		if(!Yii::$app->has($bundleName)) {
