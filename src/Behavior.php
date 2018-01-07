3a7e2fa4aaf5570d5339e71ac77b625a6a2852bd
Vitaliy <theyamshikov@yandex.ru>
2018-01-07 14:17:10 +0600

    make behavior helper

---
+++ src/Behavior.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace yii2lab\helpers;
+
+use yii\filters\AccessControl;
+use yii\filters\Cors;
+use yii\filters\VerbFilter;
+use yii\helpers\ArrayHelper;
+use yii2woop\account\domain\filters\auth\HttpTokenAuth;
+
+class Behavior {
+	
+	static function modifyActions() {
+		return ['create', 'update', 'delete'];
+	}
+	
+	static function apiAuth($only = null) {
+		$config = [
+			'class' => HttpTokenAuth::className(),
+		];
+		if(!empty($only)) {
+			$config['only'] = ArrayHelper::toArray($only);
+		}
+		return $config;
+	}
+	
+	static function verb($actions) {
+		foreach($actions as $actionName => &$actionMethods) {
+			$actionMethods = ArrayHelper::toArray($actionMethods);
+		}
+		$config = [
+			'class' => VerbFilter::className(),
+			'actions' => $actions,
+		];
+		return $config;
+	}
+	
+	static function access($roles, $only = null, $allow = true) {
+		$roles = is_array($roles) ? $roles : [$roles];
+		$config = [
+			'class' => AccessControl::className(),
+			'rules' => [
+				[
+					'allow' => $allow,
+					'roles' => $roles,
+				],
+			],
+		];
+		if(!empty($only)) {
+			$config['only'] = ArrayHelper::toArray($only);
+		}
+		return $config;
+	}
+	
+	static function cors() {
+		$origin = [];
+		$urls = env('url');
+		foreach($urls as $url) {
+			$origin[] = trim($url, SL);
+		}
+		// todo: load from env.php
+		return [
+			'class' => Cors::className(),
+			'cors' => [
+				'Origin' => $origin,
+				'Access-Control-Request-Method' => ['get', 'post', 'put', 'delete', 'options'],
+				'Access-Control-Request-Headers' => [
+					//'X-Wsse',
+					'content-type',
+					'x-requested-with',
+					'authorization',
+					'registration-token',
+				],
+				//'Access-Control-Allow-Credentials' => true,
+				//'Access-Control-Max-Age' => 3600, // Allow OPTIONS caching
+				
+				'Access-Control-Expose-Headers' => [
+					'link',
+					'access-token',
+					'authorization',
+					'x-pagination-total-count',
+					'x-pagination-page-count',
+					'x-pagination-current-page',
+					'x-pagination-per-page',
+				],
+			],
+		];
+	}
+	
+}
