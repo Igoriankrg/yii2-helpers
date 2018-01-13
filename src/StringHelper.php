dcb1f5a01c71ef60b3da998cc8e336c7400d4bbc
Vitaliy <theyamshikov@yandex.ru>
2018-01-13 21:38:45 +0600

    make methods

--- src/StringHelper.php
+++ src/StringHelper.php
@@ -4,6 +4,21 @@ namespace yii2lab\helpers;
 
 class StringHelper {
 
+	static function textToLine($text) {
+		$text = preg_replace('#\s+#m', SPC, $text);
+		return $text;
+	}
+	
+	static function removeDoubleSpace($text) {
+		$text = preg_replace('# +#m', SPC, $text);
+		return $text;
+	}
+	
+	static function textToArray($text) {
+		$text = self::removeDoubleSpace($text);
+		return explode(SPC, $text);
+	}
+	
 	static function mask($value, $length = 2, $valueLength = null) {
 		if(empty($value)) {
 			return '';
