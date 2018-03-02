6089a18d30a8f492ac473c72c07c45f56727b1a7
Vitaliy <theyamshikov@yandex.ru>
2018-03-02 10:52:31 +0600

    make base registry

--- src/Registry.php
+++ src/Registry.php
@@ -2,46 +2,8 @@
 
 namespace yii2lab\helpers;
 
-class Registry {
+class Registry extends BaseRegistry {
 	
-	private static $data = [];
-	
-	static function get($key = null, $default = null) {
-		if(empty($key)) {
-			return self::$data;
-		}
-		if(array_key_exists($key, self::$data)) {
-			return self::$data[$key];
-		} else {
-			return $default;
-		}
-	}
-	
-	static function has($key) {
-		if(empty($key)) {
-			return false;
-		}
-		if(array_key_exists($key, self::$data)) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-	
-	static function set($key, $value) {
-		if(!empty($key)) {
-			self::$data[$key] = $value;
-		}
-	}
-	
-	static function remove($key) {
-		if(!empty($key) && array_key_exists($key, self::$data)) {
-			unset(self::$data[$key]);
-		}
-	}
-	
-	protected function __construct() {
-	
-	}
+	protected static $data = [];
 	
 }
