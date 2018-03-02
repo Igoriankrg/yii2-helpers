f1bc175dc2905d59ea535deedaa8072f7d7909da
Vitaliy <theyamshikov@yandex.ru>
2018-03-02 10:53:03 +0600

    update dockblock

--- src/MenuHelper.php
+++ src/MenuHelper.php
@@ -6,7 +6,7 @@ namespace yii2lab\helpers;
  * Class MenuHelper
  *
  * @package yii2lab\helpers
- * @deprecated use  yii2lab\extension\menu\helpers\MenuHelper
+ * @deprecated use yii2lab\extension\menu\helpers\MenuHelper
  */
 class MenuHelper extends \yii2lab\extension\menu\helpers\MenuHelper
 {
--- src/interfaces/MenuInterface.php
+++ src/interfaces/MenuInterface.php
@@ -2,6 +2,12 @@
 
 namespace yii2lab\helpers\interfaces;
 
+/**
+ * Interface MenuInterface
+ *
+ * @package yii2lab\helpers\interfaces
+ * @deprecated use yii2lab\extension\menu\interfaces\MenuInterface
+ */
 interface MenuInterface {
 	
 	public function toArray();
