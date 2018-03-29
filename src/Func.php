1ff0dabffcfbd3596fdb2de5932607e39e5cc6e4
Vitaliy <theyamshikov@yandex.ru>
2018-03-29 11:13:21 +0600

    clean

--- src/Func.php
+++ src/Func.php
@@ -6,20 +6,18 @@ function prr($val, $exit = false, $forceToArray = false) {
 	Debug::prr($val, $exit, $forceToArray);
 }
 
-function mb_ucfirst ($word)
-{
+function mb_ucfirst($word) {
 	return mb_strtoupper(mb_substr($word, 0, 1, 'UTF-8'), 'UTF-8') . mb_substr(mb_convert_case($word, MB_CASE_LOWER, 'UTF-8'), 1, mb_strlen($word), 'UTF-8');
 }
 
-function sortByLen($a, $b)
-{
-  if (strlen($a) < strlen($b)) {
-  return 1;
-  }
-  elseif (strlen($a) == strlen($b)) {
-  return 0;
-  }
-  else { return -1; }
+function sortByLen($a, $b) {
+	if(strlen($a) < strlen($b)) {
+		return 1;
+	} elseif(strlen($a) == strlen($b)) {
+		return 0;
+	} else {
+		return -1;
+	}
 }
 
 /**
