e211330e4b687da7d6bc93090c160fea13f22a3e
Vitaliy <theyamshikov@yandex.ru>
2018-04-01 23:35:07 +0600

    upgrade

--- src/TempHelper.php
+++ src/TempHelper.php
@@ -3,20 +3,42 @@
 namespace yii2lab\helpers;
 
 use Yii;
+use yii\web\UploadedFile;
 use yii2lab\helpers\yii\FileHelper;
 
 class TempHelper {
 	
-	public static function fullName($name) {
-		$fullName = self::basePath() . DS . $name;
-		$fullName = FileHelper::normalizePath($fullName);
-		$directory = FileHelper::up($fullName);
-		FileHelper::createDirectory($directory);
-		return $fullName;
+	const TEMP_ALIAS = '@runtime/temp';
+	
+	public static function remove($name) {
+		$fileName = self::fullName($name);
+		FileHelper::remove($fileName);
 	}
 	
-	public static function basePath() {
-		return Yii::getAlias('@runtime/temp');
+	public static function fullName($name) {
+		$fileName = self::basePath($name);
+		self::createDirectoryForFile($fileName);
+		return $fileName;
+	}
+	
+	public static function createDirectoryForFile($fileName) {
+		$directory = FileHelper::up($fileName);
+		FileHelper::createDirectory($directory);
+	}
+	
+	public static function copyUploadedToTemp(UploadedFile $uploaded) {
+		$tempFile = self::fullName($uploaded->name);
+		$uploaded->saveAs($tempFile);
+		return $tempFile;
+	}
+	
+	public static function basePath($path = null) {
+		$basePath = Yii::getAlias(self::TEMP_ALIAS) . DS . MICRO_TIME;
+		if($path) {
+			$basePath .= DS . $path;
+		}
+		$basePath = FileHelper::normalizePath($basePath);
+		return $basePath;
 	}
 	
 	public static function clearAll() {
