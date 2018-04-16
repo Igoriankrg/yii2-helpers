541186e34acea9a47329568c9a787d3cdff6d84c
Vitaliy <theyamshikov@yandex.ru>
2018-04-16 21:45:40 +0600

    fix api version

--- src/ApiVersionCofig.php
+++ src/ApiVersionCofig.php
@@ -4,21 +4,12 @@ namespace yii2lab\helpers;
 
 use yii\helpers\ArrayHelper;
 
-class ApiVersionCofig {
-	
-	static function configFileName($name) {
-		$versionConfig = ROOT_DIR . DS . 'api' . DS . API_VERSION_STRING . DS . 'config' . DS . $name . '.php';
-		return $versionConfig;
-	}
-	
-	static function load($name, $config) {
-		if(defined('API_VERSION') && API_VERSION) {
-			$configFileName = self::configFileName($name);
-			$versionConfig = include($configFileName);
-			return ArrayHelper::merge($config, $versionConfig);
-		} else {
-			return $config;
-		}
-	}
-	
+/**
+ * Class ApiVersionCofig
+ * @package yii2lab\helpers
+ *
+ * @deprecated use ApiVersionConfig
+ */
+class ApiVersionCofig extends ApiVersionConfig {
+
 }
