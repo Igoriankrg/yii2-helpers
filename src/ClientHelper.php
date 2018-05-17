2565bb6676382282c5178b34301647fd9e40b23e
Vitaliy <theyamshikov@yandex.ru>
2018-05-17 13:27:02 +0600

    fix

--- src/ClientHelper.php
+++ src/ClientHelper.php
@@ -13,17 +13,16 @@ class ClientHelper
         if (APP == CONSOLE) {
             return '127.0.0.1';
         }
-	    if (APP == API) {
-		    $headerIp = Yii::$app->request->headers->get(self::IP_HEADER_KEY, false);
-		    if($headerIp) {
-			    return $headerIp;
-		    }
-	    }
-        if ($_SERVER['REMOTE_ADDR'] == env('servers.nat.address') && isset($_SERVER['HTTP_CLIENT_IP'])) {
+       /* if ($_SERVER['REMOTE_ADDR'] == env('servers.nat.address') && isset($_SERVER['HTTP_CLIENT_IP'])) {
             $clientIp = $_SERVER['HTTP_CLIENT_IP'];
         } else {
             $clientIp = $_SERVER['REMOTE_ADDR'];
-        }
+        }*/
+	    $clientIp = Yii::$app->request->userIP;
+	    $headerIp = Yii::$app->request->headers->get(self::IP_HEADER_KEY, false);
+	    if($headerIp) {
+		    $clientIp = $headerIp;
+	    }
         return $clientIp;
     }
 
