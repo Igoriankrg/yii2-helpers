1d0f8264e53aecea32f92b469187b2637bf76633
Vitaliy <theyamshikov@yandex.ru>
2018-08-10 18:38:08 +0600

    make method

--- src/StringHelper.php
+++ src/StringHelper.php
@@ -6,6 +6,12 @@ class StringHelper {
 	
 	const PATTERN_SPACES = '#\s+#m';
 	
+	public static function stripContent($data, $beginText, $endText) {
+		$pattern = preg_quote($beginText) . '[\s\S]+' . preg_quote($endText);
+		$data = preg_replace('#' . $pattern . '#i', EMP, $data);
+		return $data;
+	}
+	
 	public static function genUuid() {
 		$uuid = array(
 			'time_low'  => 0,
