9b32f00c6caff9b03197bceba92172bf93428093
Vitaliy <theyamshikov@yandex.ru>
2018-10-04 20:13:27 +0600

    refacto

--- src/ReflectionHelper.php
+++ src/ReflectionHelper.php
@@ -2,45 +2,12 @@
 
 namespace yii2lab\helpers;
 
-use ReflectionClass;
+/**
+ * Class ReflectionHelper
+ *
+ * @package yii2lab\helpers
+ * @deprecated
+ */
+class ReflectionHelper extends \yii2lab\extension\common\helpers\ReflectionHelper {
 
-class ReflectionHelper {
-	
-	public static function getConstantsByPrefix($class, $prefix) {
-		$constants = self::getConstants($class);
-		return self::filterByPrefix($constants, $prefix);
-	}
-	
-	public static function getConstantsValuesByPrefix($class, $prefix) {
-		$constants = self::getConstantsByPrefix($class, $prefix);
-		return array_values($constants);
-	}
-	
-	public static function getConstantsNamesByPrefix($class, $prefix) {
-		$constants = self::getConstantsByPrefix($class, $prefix);
-		return array_keys($constants);
-	}
-	
-	public static function getConstants($class) {
-		$class = new ReflectionClass($class);
-		$constants = $class->getConstants();
-		return $constants;
-	}
-	
-	private static function filterByPrefix($constants, $prefix) {
-		$result = [];
-		foreach($constants as $name => $value) {
-			if(self::isNameWithPrefix($prefix, $name)) {
-				$result[ $name ] = $value;
-			}
-		}
-		return $result;
-	}
-	
-	private static function isNameWithPrefix($prefix, $name) {
-		$ucPrefix = strtoupper($prefix);
-		$ucPrefixWithBl = $ucPrefix . BL;
-		return strpos($name, $ucPrefixWithBl) === 0;
-	}
-	
 }
